59accedfa306c0f0db5d787adfcc7ef75e074546
pereguda-anatoliy <pereguda@gmail.com>
2025-03-04 15:00:06 +0500

    feat: add 12.go 12.md

--- 12/12.go
+++ 12/12.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+const (
+	CodeOK = iota
+	CodeCancelled
+	CodeUnknown
+)
+
+func ErrorMessageToCode(msg string) int {
+	switch msg {
+	case "OK":
+		return CodeOK
+	case "CANCELLED":
+		return CodeCancelled
+	case "UNKNOWN":
+		return CodeUnknown
+	default:
+		return CodeUnknown
+	}
+}
+
+func main() {
+	fmt.Println(ErrorMessageToCode("OK"))        // 0
+	fmt.Println(ErrorMessageToCode("CANCELLED")) // 1
+	fmt.Println(ErrorMessageToCode("UNKNOWN"))   // 2
+	fmt.Println(ErrorMessageToCode("ANY"))       // 2
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module dacode
 
 go 1.24.0
 
-require golang.org/x/text v0.22.0 // indirect
+require golang.org/x/text v0.22.0
